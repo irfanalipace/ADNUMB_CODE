2063890677e60e8347cb106a07066e503438ad67
aftab ahmad <aftab.ahmad334@gmail.com>
2023-03-08 19:52:01 +0500

    stag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './components/LoginPage';
 import ForgetEmail from './components/login/ForgetEmail';
 import ResetEmail from './components/login/ResetEmail';
 import Form from './components/login/Form';
-import SiginUp from './components/login/SiginUp';
+
 function App() {
   return (
     <div className="App">

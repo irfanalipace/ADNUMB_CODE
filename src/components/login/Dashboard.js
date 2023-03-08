7b7f99a600ab8401ca9d64f73dca62ebf9a5ff12
Irfan Ali <irfan.ali@99technologies.co>
2023-03-08 14:36:08 +0000

    Inventory dashboard frontend almost done

--- src/components/login/Dashboard.js
+++ src/components/login/Dashboard.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import TblFunc from './TblFunc'
+import Data from './Data'
+import img1 from '../../image/icon/fi-file-plus.png';
+import img2 from '../../image/icon/Bag.png';
+import img3 from '../../image/icon/User.png';
+
+import img4 from '../../image/icon/folder.png';
+import img5 from '../../image/icon/Chat.png';
+import img6 from '../../image/icon/Setting.png';
+import img7 from '../../image/icon/fi-headphone.png';
+import img8 from '../../image/icon/fi-gift.png';
+import img9 from '../../image/icon/fi_chevron.png';
+import '../../styles.css'
+
 const Dashboard = () => {
 
   // const dataSet = [
@@ -14,56 +27,80 @@ const Dashboard = () => {
     <div className="container-fluid">
       <div className="row">
 
-        <div className="col-md-2">
+        <div className="col-md-2 ">
 
 
           <div className="sidebar">
 
             <div className='formatter'>
-              <button className='btn btn-success chet-inventoy'>  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ui-checks-grid" viewBox="0 0 16 16">
+              <button className='btn btn-success chet-inventoy'>  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-ui-checks-grid" viewBox="0 0 16 16">
                 <path d="M2 10h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1zm9-9h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zm0 9a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-3zm0-10a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-3zM2 9a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H2zm7 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2v-3zM0 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.354.854a.5.5 0 1 0-.708-.708L3 3.793l-.646-.647a.5.5 0 1 0-.708.708l1 1a.5.5 0 0 0 .708 0l2-2z" />
               </svg>
                {' '}{' '} Inventroy Formatter
               </button>
 
             </div>
+
+
             <div className='menu'>
 
 
-              <a href="#home"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag-dash" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
-                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-              </svg>{' '}{' '}Orders</a>
-              <a href="#services"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
-                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
-              </svg> Customer</a>
-              <a href="#services"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
-                <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
-              </svg> Inventory</a>
-              <a href="#clients"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
-                <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
-              </svg> Conversation</a>
-              <a href="#contact"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
-                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
-                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
-              </svg> setting</a>
+
+
+            <ul>
+               
+                    <a href="#" className="active orders">
+                        <span className="icon"><img src={img2} alt=''/></span>
+                        <span className="item">Orders</span>
+                    </a>
+            
+            
+                    <a href="#" className='customer'>
+                        <span className="icon"><img src={img3} alt=''/></span>
+                        <span className="item">Customer</span>
+                    </a>
+             
+               
+                    <a href="#" className='inventory-menu'>
+                        <span className="icon"><img src={img4} alt=''/></span>
+                        <span className="item">Inventory</span>
+                    </a>
+             
+              
+                    <a href="#" className='conversation'>
+                        <span className="icon"><img src={img5} alt=''/></span>
+                        <span className="item">Conversation</span>
+                    </a>
+              
+             
+                    <a href="#" className='setting'>
+                        <span className="icon"><img src={img6} alt=''/></span>
+                        <span className="item">Setting</span>
+                    </a>
+             
+              
+              
+            </ul>
+      
+          
+          {/* <a> <img src={img3} alt=''/>Customer</a> 
+          <a> <img src={img4} alt=''/>Inventory</a> 
+          <a> <img src={img5} alt=''/>Conversation</a> 
+          <a> <img src={img6} alt=''/>Setting</a> 
+           */}
             </div>
             <div>
 
 
-              <button className='btn btn-success headphone'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-headphones" viewBox="0 0 16 16">
-                  <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5z" />
-                </svg> Contact Support
+              <button className='btn btn-success headphone' style={{background: 'rgba(94, 99, 102, 0.1)', 'border-radius': '10px', color:'black'}}>
+                <img src ={img7}/> Contact Support
               </button>
 
               <button className='btn btn-success gift'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gift" viewBox="0 0 16 16">
-                  <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z" />
-                </svg>{' '}free Gift Awaits You!<br></br><br></br>
-                upgrade your account{' '}
-                <i className="fa fa-chevron-right fa-0x"></i>
+              <img src ={img8}/>
+               {' '}free Gift Awaits You!
+                upgrade your account
+               <img src={img9} />
               </button>
              
               <button className=' button-logout'>
@@ -127,11 +164,12 @@ const Dashboard = () => {
 
 
                 <button className="w3-btn w3xlarge">
-                  <i className="fa fa-file fa-icon-size fa-4x">
+                  <img src={img1} alt='' />
+                  {/* <i className="fa fa-file fa-icon-size fa-4x"> */}
                   <div className='center'>
                     <input type="file" name="file" className='custom-file-input' />
                   </div>
-                  </i>
+                  {/* </i> */}
                 </button>
 
                 <div className='download'>
@@ -155,8 +193,7 @@ const Dashboard = () => {
 
 
               <div className='datatable'>
-                <TblFunc />
-
+              <Data />
               </div>
             </div>
           </div>
--- src/components/login/Data.js
+++ src/components/login/Data.js
@@ -1,59 +1,197 @@
-import React,{useState} from 'react'
-
-const data = [
-  {
-      "name": "committee-c15dw",
-     
-  },
-  {
-      "name": "midnight-wad0y",
-     
-  }]
 
 
+
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
+import React, { useEffect, useRef } from 'react';
+import '../../styles.css'
  const Data = () => {
-  const [query, setQuery] = useState("");
-const [sort, setSort] = useState("asc");
-const [page, setPage] = useState(1);
-function handleQueryChange(event) {
-  setQuery(event.target.value);
-  setPage(1); // reset page to 1 when search query changes
-}
-function handleSortChange(event) {
-  setSort(event.target.value);
-  setPage(1); // reset page to 1 when sort order changes
-}
-function getFilteredAndSortedData() {
-  let filteredData = data.filter(item => item.name.includes(query));
-  if (sort === "asc") {
-    filteredData = filteredData.sort((a, b) => a.name.localeCompare(b.name));
-  } else {
-    filteredData = filteredData.sort((a, b) => b.name.localeCompare(a.name));
-  }
-  return filteredData;
-}
-const itemsPerPage = 10;
-const startIndex = (page - 1) * itemsPerPage;
-const endIndex = startIndex + itemsPerPage;
-const filteredAndSortedData = getFilteredAndSortedData();
-const paginatedData = filteredAndSortedData.slice(startIndex, endIndex);
+  const tableRef = useRef(null);
 
-{paginatedData.map(item => (
-  <div key={item.id}>{item.name}</div>
-))}
+  useEffect(() => {
+    const $table = $(tableRef.current);
+    $table.DataTable();
+  }, []);
+
+///javascript code 
+
+
   return (
-    <div className='container-fluid'>
-        <div className='row'>
-         <div className='col-md-12'>
-         <input type="text" value={query} onChange={handleQueryChange} />
-<select value={sort} onChange={handleSortChange}>
-  <option value="asc">A-Z</option>
-  <option value="desc">Z-A</option>
-</select>
+   
+         <div className='search-bar'>
+   
+         <table id="myTable" ref={tableRef} cellSpacing="0" className="table table-striped table-bordered table-sm">
+  <thead>
+    <tr>
+      <th className="th-sm">File Name
+
+      </th>
+      <th className="th-sm">Created at
+
+      </th>
+      <th className="th-sm">User
+
+      </th>
+      <th className="th-sm">Action
+
+      </th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td><button className="btn btn-success download-data"><i className="fa fa-download"></i> Download</button></td>
+      
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td><button className="btn btn-primary download-data"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Cara Stevens</td>
+      <td>Sales Assistant</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+      
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Cara Stevens</td>
+      <td>Sales Assistant</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Zenaida Frank</td>
+      <td>Software Engineer</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Zorita Serrano</td>
+      <td>Software Engineer</td>
+      <td>San Francisco</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+      
+    </tr>
+    <tr>
+      <td>Cara Stevens</td>
+      <td>Sales Assistant</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Jennifer Acosta</td>
+      <td>Junior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    </tr>
+    <tr>
+      <td>Cara Stevens</td>
+      <td>Sales Assistant</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Hermione Butler</td>
+      <td>Regional Director</td>
+      <td>London</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Donna Snider</td>
+      <td>Customer Support</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Lael Greer</td>
+      <td>Systems Administrator</td>
+      <td>London</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Jonas Alexander</td>
+      <td>Developer</td>
+      <td>San Francisco</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+      
+    </tr>
+    <tr>
+      <td>Donna Snider</td>
+      <td>Customer Support</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Shad Decker</td>
+      <td>Regional Director</td>
+      <td>Edinburgh</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Michael Bruce</td>
+      <td>Javascript Developer</td>
+      <td>Singapore</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+    
+    </tr>
+    <tr>
+      <td>Donna Snider</td>
+      <td>Customer Support</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+    <tr>
+      <td>Donna Snider</td>
+      <td>Customer Support</td>
+      <td>New York</td>
+      <td><button className="btn btn-primary"><i className="fa fa-download"></i> Download</button></td>
+     
+    </tr>
+  </tbody>
+ 
+</table>
 
          </div>
-        </div>
-    </div>
+     
   )
 }
 export default Data

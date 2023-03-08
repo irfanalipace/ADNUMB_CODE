de98fcd2d498cc25c0c835bb5d74590147d6d7b8
aftab ahmad <aftab.ahmad334@gmail.com>
2023-03-08 20:12:46 +0500

    code refactored

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import LoginPage from './components/LoginPage';
 import ForgetEmail from './components/login/ForgetEmail';
 import ResetEmail from './components/login/ResetEmail';
 import Form from './components/login/Form';
-<<<<<<< HEAD
-
-=======
 import Dashboard from './components/login/Dashboard';
 import Data from './components/login/Data';
->>>>>>> 7b7f99a600ab8401ca9d64f73dca62ebf9a5ff12
 function App() {
   return (
     <div className="App">
